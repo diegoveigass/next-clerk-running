e813a1f9dadcd025d46a02abf14384168c3cbd6c
Diego Veiga <sdiegoveiga@gmail.com>
2025-04-02 18:25:09 -0300

    Initializing form schema with react-hook-form and zod

--- src/components/create-running-dialog.tsx
+++ src/components/create-running-dialog.tsx
@@ -13,8 +13,48 @@ import { Button } from "@/components/ui/button";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const timeRegex = /^((\d+h)?(\d+m)?(\d+s)?)$/;
+
+const createNewRunningSchema = z.object({
+	time: z
+		.string()
+		.regex(timeRegex, "Formato inválido. Use algo como '1h33m21s'")
+		.transform((value) => {
+			const match = value.match(/(\d+h)?(\d+m)?(\d+s)?/);
+			if (!match) return 0;
+
+			const hours = match[1]
+				? Number.parseInt(match[1].replace("h", "")) * 3600
+				: 0;
+			const minutes = match[2]
+				? Number.parseInt(match[2].replace("m", "")) * 60
+				: 0;
+			const seconds = match[3] ? Number.parseInt(match[3].replace("s", "")) : 0;
+
+			return hours + minutes + seconds;
+		}),
+});
+
+type FormValues = z.infer<typeof createNewRunningSchema>;
+
 export default function CreateRunningDialog() {
 	const [isOpen, setIsOpen] = useState(false);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(createNewRunningSchema),
+	});
+
+	function handleCreateNewRunning(data: FormValues) {
+		console.log(data);
+	}
 
 	return (
 		<Dialog open={isOpen} onOpenChange={setIsOpen}>
@@ -28,13 +68,25 @@ export default function CreateRunningDialog() {
 						Aqui é onde você irá adicionar a sua melhor corrida :)
 					</DialogDescription>
 				</DialogHeader>
-				<div className="grid gap-4 py-4">
+				<form
+					className="grid gap-4 py-4"
+					onSubmit={handleSubmit(handleCreateNewRunning)}
+				>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="time" className="text-right">
 							Tempo
 						</Label>
-						<Input id="time" defaultValue="33m25s" className="col-span-3" />
+						<Input
+							id="time"
+							defaultValue="33m25s"
+							data-error={!!errors.time}
+							className="col-span-3 data-[error=true]:border data-[error=true]:border-red-500"
+							{...register("time")}
+						/>
 					</div>
+					{errors.time && (
+						<p className="text-sm text-red-500">{errors.time.message}</p>
+					)}
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="distance">Distância percorrida</Label>
 						<Input id="distance" defaultValue="4.25" className="col-span-3" />
@@ -54,8 +106,8 @@ export default function CreateRunningDialog() {
 						<Label htmlFor="date">Data da corrida</Label>
 						<Input id="date" type="datetime-local" className="col-span-3" />
 					</div>
-				</div>
-				<Button>Criar</Button>
+					<Button type="submit">Criar</Button>
+				</form>
 			</DialogContent>
 		</Dialog>
 	);
